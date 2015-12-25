aca54d383cb0f7796715d426b4e8f2dd81c562f2
db71 <dennisbakker71@outlook.com>
2015-12-25 13:17:23 +0100

    Upadte 3

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -25,9 +25,9 @@ namespace Hamburger__menu_swipe_in
 		#region "Hamburger menu"
 
 		double stckpnlMenuWidth = 210;
-		double InitialManipulationPoint;
+		double InitialManipulationPointX;
 		bool HamburgerMenuOpen = false;
-		bool TriggerManipulation = false;
+		bool HamburgerMenuOpeningClosing = false;
 
 		private void InitializeHamburgerMenu()
 		{
@@ -102,54 +102,52 @@ namespace Hamburger__menu_swipe_in
 
 		private void pgMainPage_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
 		{
-			InitialManipulationPoint = e.Position.X;
-			TriggerManipulation = HamburgerMenuOpen ? false : InitialManipulationPoint < 30 ? true : false;
+			InitialManipulationPointX = e.Position.X;
+			HamburgerMenuOpeningClosing = HamburgerMenuOpen ? false : InitialManipulationPointX < 30 ? true : false;
 		}
 
 		private void pgMainPage_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
 		{
 
-			if ((TriggerManipulation || HamburgerMenuOpen) && (!(HamburgerMenuOpen && InitialManipulationPoint < e.Position.X) || TriggerManipulation))
+			if (HamburgerMenuOpeningClosing || (HamburgerMenuOpen && InitialManipulationPointX > e.Position.X))
 			{
+				HamburgerMenuOpeningClosing = true;
 				if (e.Position.X < stckpnlMenuWidth + 1)
 				{
-					HamburgerMenuOpen = true;
 					Point currentpoint = e.Position;
 					trnslttrnsfrmMenuTop.X = e.Position.X < stckpnlMenuWidth ? -stckpnlMenuWidth + e.Position.X : 0;
 					trnslttrnsfrmMenuBottom.X = e.Position.X < stckpnlMenuWidth ? -stckpnlMenuWidth + e.Position.X : 0;
 				}
 			}
-		}
+		} 
 
 		private async void pgMainPage_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
 		{
-			if (TriggerManipulation || HamburgerMenuOpen)
+			if (HamburgerMenuOpeningClosing || (HamburgerMenuOpen && InitialManipulationPointX > e.Position.X))
 			{
-				if ((!HamburgerMenuOpen && InitialManipulationPointX < e.Position.X) || HamburgerMenuOpen)
+				double X = e.Position.X > stckpnlMenuWidth ? stckpnlMenuWidth : e.Position.X;
+				if (X > stckpnlMenuWidth / 2)
 				{
-					double X = e.Position.X > stckpnlMenuWidth ? stckpnlMenuWidth : e.Position.X;
-					if (X > stckpnlMenuWidth / 2)
-					{
-						shMenu1.From = -stckpnlMenuWidth + X;
-						shMenu2.From = -stckpnlMenuWidth + X;
-						await strbrdShowMenu.BeginAsync();
-						shMenu1.From = -stckpnlMenuWidth;
-						shMenu2.From = -stckpnlMenuWidth;
-						grdPageOverlay.Visibility = Visibility.Visible;
-					}
-					else	
-					{	
-						grdPageOverlay.Visibility = Visibility.Collapsed;
-						hdMenu1.From = X - stckpnlMenuWidth;
-						hdMenu2.From = X - stckpnlMenuWidth;
-						await strbrdHideMenu.BeginAsync();
-						hdMenu1.From = 0;
-						hdMenu2.From = 0;
-						HamburgerMenuOpen = false;
-					}
+					shMenu1.From = -stckpnlMenuWidth + X;
+					shMenu2.From = -stckpnlMenuWidth + X;
+					await strbrdShowMenu.BeginAsync();
+					shMenu1.From = -stckpnlMenuWidth;
+					shMenu2.From = -stckpnlMenuWidth;
+					grdPageOverlay.Visibility = Visibility.Visible;
+					HamburgerMenuOpen = true;
+				}
+				else	
+				{	
+					grdPageOverlay.Visibility = Visibility.Collapsed;
+					hdMenu1.From = X - stckpnlMenuWidth;
+					hdMenu2.From = X - stckpnlMenuWidth;
+					await strbrdHideMenu.BeginAsync();
+					hdMenu1.From = 0;
+					hdMenu2.From = 0;
+					HamburgerMenuOpen = false;
 				}
 			}
-			TriggerManipulation = false;
+			HamburgerMenuOpeningClosing = false;
 		}
 
 		#endregion
